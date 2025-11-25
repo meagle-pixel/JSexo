d24969b8b00458eb009309a6406bf6be90734d6a
meagle-pixel <m.eagle@hotmail.fr>
2025-11-25 14:54:48 +0100

    firstcommit

---
+++ exo01.js
@@ -0,0 +1,9 @@
+const Name = "PixelBay";
+let stock = 500;
+let moy_prix = 15;
+console.log("La boutique s'appelle " + Name);
+console.log("Le stock de jeux vidéos au début est de " + stock);
+let quant_vendu = stock * 0.3;
+console.log("Après les 30% de jeux vendus, il n'en reste désormais plus que " + quant_vendu); 
+let money_win = moy_prix * quant_vendu;
+console.log("Le CA de ces ventes est de " + money_win + " euros.");
