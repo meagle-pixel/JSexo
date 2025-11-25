676c310683412ab003d031ea8a217ce0fe2c9074
meagle-pixel <m.eagle@hotmail.fr>
2025-11-25 16:40:22 +0100

    ajoutexo5

--- exo01.js
+++ exo01.js
@@ -1,8 +1,8 @@
 const Name = "PixelBay";
 let stock = 500;
 let moy_prix = 15;
-console.log("La boutique s'appelle " + Name);
-console.log("Le stock de jeux vidéos au début est de " + stock);
+console.log(`La boutique s'appelle ${Name}`);    // On peut calculer de cette manière avec "`" c'est préférable
+console.log(`Le stock de jeux vidéos au début est de ${stock}`);    
 let quant_vendu = stock * 0.3;
 console.log("Après les 30% de jeux vendus, il n'en reste désormais plus que " + quant_vendu); 
 let money_win = moy_prix * quant_vendu;
---
+++ exo04.js
@@ -0,0 +1,12 @@
+let days = [
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+  "Dimanche",
+];
+for (let index = 0; index < days.length; index++) {
+  console.log(days[index]);
+}
