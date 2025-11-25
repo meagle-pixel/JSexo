fa7de1c6a9b9d7c2b60dddf4366c5be4a2704c9d
meagle-pixel <m.eagle@hotmail.fr>
2025-11-25 14:58:11 +0100

    JSexo modif1

--- exo01.js
+++ exo01.js
@@ -6,4 +6,4 @@ console.log("Le stock de jeux vidéos au début est de " + stock);
 let quant_vendu = stock * 0.3;
 console.log("Après les 30% de jeux vendus, il n'en reste désormais plus que " + quant_vendu); 
 let money_win = moy_prix * quant_vendu;
-console.log("Le CA de ces ventes est de " + money_win + " euros.");
+console.log("Le CA de ces ventes est de " + money_win + " €.");
