63f7bc7310c1b563cf9367e2d84a1e151daef1bc
meagle-pixel <m.eagle@hotmail.fr>
2025-11-25 15:06:21 +0100

    modifexo2

---
+++ exo02.js
@@ -0,0 +1,4 @@
+let array = ["AC", "FarCry", "GTA", "Mafia", "Star Citizen"]
+console.log("Le deuxième jeu de la liste est " + array [1]);
+array [2] = ["Clair Obscur"]
+console.log("Le nouveau nom de Far Cry est " + array[2]);
