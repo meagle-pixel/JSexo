95f558377c3e999729f05ac885d24fe922e82af3
meagle-pixel <m.eagle@hotmail.fr>
2025-11-26 11:59:28 +0100

    commit8

---
+++ exo08.js
@@ -0,0 +1,16 @@
+
+let prixJeu = 50;
+
+function promotionEte(prix) {
+    return prix*0.8
+}
+
+function promotionHiver(prix) {
+    return prix*0.7
+}
+
+function promotionSpeciale (prix) {
+    return
+}
+
+console.log();
