59a37df57fb581c14664289bb8cbeeb311b8e599
BlazeAttitude <m.eagle@hotmail.fr>
2025-12-01 22:44:19 +0100

    jusqua14

--- exo13.js
+++ exo13.js
@@ -2,25 +2,25 @@ let collection = [
   {
     titre: "Space Adventure",
     anneeSortie: 1984,
-    scores: [85, 87, 90, 78, 88],
+    score: [85, 87, 90, 78, 88],
   },
   {
     titre: "FarCry",
     anneeSortie: 1984,
-    scores: [90, 95, 99, 99, 94],
+    score: [90, 95, 99, 99, 94],
   },
   {
     titre: "Prince of Persia",
     anneeSortie: 1984,
-    scores: [59, 75, 80, 68, 74],
+    score: [59, 75, 80, 68, 74],
   },
 ];
 
 function afficher(jeu) {
   console.log("*** JEU ***");
-  console.log("Titre :", jeu.titre);
-  console.log("Année de sortie", jeu.anneeSortie);
-  console.log("Scores :", jeu.scores);
+  console.log(`Titre : ${jeu.titre}`)
+  console.log(`Année : ${jeu.anneeSortie}`);
+  console.log(`Score : ${jeu.score}`)
 }
 
 function afficherCollection(collection) {
@@ -31,3 +31,7 @@ function afficherCollection(collection) {
 }
 
 afficherCollection(collection);
+
+function calculerMoyenneScore (jeu) {
+  let total = jeu.score.reduce()
+}
---
+++ exo14.js
@@ -0,0 +1,30 @@
+let collection = [
+  {
+    titre: "Space Adventure",
+    anneeSortie: 1984,
+    genre: "Arcade",
+    scores: [85, 87, 90, 78, 88],
+  },
+
+  {
+    titre: "Candy Crush",
+    anneeSortie: 1994,
+    genre: "Arcade",
+    scores: [70, 65, 80, 78, 74],
+  },
+
+  {
+    titre: "Indiana Jones",
+    anneeSortie: 1999,
+    genre: "Arcade",
+    scores: [88, 81, 71, 66, 84],
+  },
+
+  {
+    titre: "Valorant",
+    anneeSortie: 2020,
+    genre: "FPS",
+    scores: [92, 91, 94, 90, 93],
+  },
+
+];
