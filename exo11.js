637b5e974732b10e4c7a8cb989409815459c9de6
meagle-pixel <m.eagle@hotmail.fr>
2025-12-02 16:35:31 +0100

    fait jusqu'à l'exercice 15 avec quelques modifications

--- exo11.js
+++ exo11.js
@@ -1,96 +1,48 @@
-
 let jeu1 = {
-    titre: "Assassin's Creed",
-    sortie: 2015,
-    genre: "RPG",
-    score: 75,
+  titre: "Assassin's Creed",
+  sortie: 2015,
+  genre: "RPG",
+  score: 75,
 
-    affichage() {
-        console.log("Titre :", this.titre);
-        console.log("Année :", this.sortie);
-        console.log("Genre :", this.genre);
-        console.log("Score :", this.score);
-    },
-        
-    getPopularite() {
-        return(this.score * this.sortie) / 100
-    }
+  affichage() {
+    console.log(`Titre : ${this.titre}`);
+    console.log(`Sortie : ${this.sortie}`);
+    console.log(`Genre : ${this.genre}`);
+    console.log(`Score : ${this.score}`);
+  },
+
+  getPopularite() {
+    return (this.score * this.sortie) / 100;
+  },
 };
 
 let jeu2 = {
-    titre: "GTA",
-    sortie: 2013,
-    genre: "RPG",
-    score: 90,
+  titre: "GTA",
+  sortie: 2013,
+  genre: "RPG",
+  score: 90,
 
-    affichage() {
-        console.log("Titre :", this.titre);
-        console.log("Année :", this.sortie);
-        console.log("Genre :", this.genre);
-        console.log("Score :", this.score);
-    },
-        
-    getPopularite () {
-        return(this.score * this.sortie) / 100
-    }
+  affichage() {
+    console.log(`Titre : ${this.titre}`);
+    console.log(`Sortie : ${this.sortie}`);
+    console.log(`Genre : ${this.genre}`);
+    console.log(`Score : ${this.score}`);
+  },
+
+  getPopularite() {
+    return (this.score * this.sortie) / 100;
+  },
 };
 
-let popularite1 = jeu1.getPopularite()
-let popularite2 = jeu2.getPopularite()
-
+let popularite1 = jeu1.getPopularite();
+let popularite2 = jeu2.getPopularite();
 
 if (popularite1 > popularite2) {
-    console.log(`Le jeu le plus populaire est ${jeu1.titre}`);
-    jeu1.affichage();
-    console.log(`Popularité : ${popularite1}`);
+  console.log(`Le jeu le plus populaire est ${jeu1.titre}`);
+  jeu1.affichage();
+  console.log(`Popularité : ${popularite1}`);
 } else {
-    console.log(`Le jeu le plus populaire est ${jeu2.titre}`);
-    jeu2.affichage();
-    console.log(`Popularité : ${popularite2}`);
+  console.log(`Le jeu le plus populaire est ${jeu2.titre}`);
+  jeu2.affichage();
+  console.log(`Popularité : ${popularite2}`);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
